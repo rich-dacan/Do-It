018b260dc1836384ac17bc63fb5b366cec9f0495
Richard Cândido <rich_dacan@protonmail.com>
2022-05-27 15:36:09 -0300

    feat :sparkles: creation of the four input business rules: standard, focus, complete and error

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { forwardRef, ForwardRefRenderFunction } from 'react'
 import { useState, useEffect, useCallback, useRef } from 'react'
 import { FormControl, FormErrorMessage, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps, InputLeftElement, InputGroup } from '@chakra-ui/react'
 import { FaExclamation} from 'react-icons/fa'
 import { FieldError } from 'react-hook-form'
 import { IconType } from 'react-icons/lib'
+import { resourceUsage } from 'process'
 
 /*  
   Usa-se o extends para herdar todas as props vindo do InputProps do Chakra UI como as estilizações por exemplo; 
@@ -14,29 +15,90 @@ interface InputProps extends ChakraInputProps {
   label?: string,
   error?: FieldError | null
   icon?: IconType,
-
 }
 
-export const Input = ({name, error = null, icon:Icon, label, ...rest}: InputProps) => {
+// Criado uma tipagem pra definir os tipos de cada elemento do objeto abaixo
+type inputVariationOptions = {
+  [key: string]: string,
+}
+
+const inputVariations: inputVariationOptions = {
+  error: 'red.500',
+  default: 'gray.200',
+  focus: 'purple.800',
+  filled: 'green.500',
+}
+
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+{ name, error = null, icon:Icon, label, ...rest }, ref 
+) => {
+
+    // const inputsRef = useRef<HTMLInputElement>(null)
+
+  const [variation, setVariation] = useState('default')
+  const [value, setValue]         = useState('')
+  
+
+  useEffect(() => {
+
+    if(error) {
+      return setVariation('error')
+    }
+
+  }, [error])
+
+  const handleInputFocus = useCallback(() => {
+  
+    if(!error) {
+      return setVariation('focus')
+    }
+
+  }, [error])
+
+  const handleInputBlur = useCallback(() => {
+
+    if(value.length > 0 && !error) {
+
+      return setVariation('filled')
+    }
+
+  }, [error, value])
+
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {/* 
           !! Converte pra boolean
           Se label existir, renderize <FormLabel/>
       */}
-      {!!label && <FormLabel>{label}</FormLabel>}
+      {!!label && <FormLabel>{ label }</FormLabel>}
 
       <InputGroup flexDirection={"column"}>
       {Icon && 
-        <InputLeftElement mt={"1"} >
+        <InputLeftElement color={ inputVariations[variation] } mt={"1"} >
           <Icon/>
         </InputLeftElement>
       }
-        <ChakraInput {...rest} name={name} bg={"gray.50"} variant={"outline"} _hover={{bg: "gray.100"}} _placeholder={{color: "gray.300", fontSize: "small"}} size={"lg"} h={"60px"} />
+        <ChakraInput 
+          ref={ref}
+          {...rest} 
+          name={name} 
+          color={ inputVariations[variation] } 
+          borderColor={ inputVariations[variation] }
+          onFocus={ handleInputFocus }
+          onBlurCapture={ handleInputBlur }
+          onChangeCapture={(e) => setValue(e.currentTarget.value)}
+          bg={"gray.50"} 
+          variant={"outline"} 
+          _hover={{bg: "gray.100"}} 
+          _placeholder={{color: "gray.300", fontSize: "small"}} 
+          size={"lg"} h={"60px"} 
+        />
 
         {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+
       </InputGroup>
 
     </FormControl>
   )
 }
+export const Input =  forwardRef(InputBase)
