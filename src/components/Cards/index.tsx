09834cebd72d06641dcf61ee8f932245122236f7
Richard Cândido <rich_dacan@protonmail.com>
2023-01-02 21:53:54 -0300

    feat :sparkles: created main card component

---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,58 @@
+import { Box, Center, Flex, Heading, Progress, Text } from "@chakra-ui/react";
+import React from "react";
+import { FaCheck, FaTrash } from "react-icons/fa";
+import { theme } from "../../styles/theme";
+
+export const MainCard: React.FC = () => {
+  return (
+    <Box
+      m={["0px", "35px 60px"]}
+      w={["90vw", "456px"]}
+      p={["10px", "20px 25px"]}
+      cursor={"pointer"}
+      transition={"border 0.2s, ease 0s, transform 0.2s"}
+      borderRadius={"5px"}
+      borderWidth={"1px"}
+      borderColor={"gray.50"}
+      boxShadow={"md"}
+      _hover={{ transform: "translateY(-7px)", borderColor: "gray.100" }}
+    >
+      <Flex>
+        <Heading as={"h2"} fontSize={"20px"}>
+          Studying database-driven concepts
+        </Heading>
+        <Flex>
+          <Center
+            as="button"
+            w={"30px"}
+            h={"30px"}
+            ml={"25px"}
+            border={"1px solid #9E9EA7"}
+            borderRadius={"5px"}
+            _hover={{ bg: "gray.100" }}
+          >
+            <FaTrash size={18} color={theme.colors.gray["300"]} />
+          </Center>
+          <Center
+            as="button"
+            w={"30px"}
+            h={"30px"}
+            ml={"25px"}
+            border={"1px solid #9E9EA7"}
+            borderRadius={"5px"}
+            _hover={{ bg: "gray.100" }}
+          >
+            <FaCheck size={18} color={theme.colors.gray["300"]} />
+          </Center>
+        </Flex>
+      </Flex>
+      <Text color={"gray.400"} fontSize={"14px"} mt={"15px"}>
+        Start study through Kenzie app, for 1 hour and a half
+      </Text>
+      <Progress size="sm" isIndeterminate mt={"15px"} colorScheme={"purple"} />
+      <Text color={"gray.300"} fontSize={"12px"} mt={"15px"}>
+        07 March 2021
+      </Text>
+    </Box>
+  );
+};
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,32 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Grid } from "@chakra-ui/react";
 import React from "react";
 
 import { Header } from "../../components/Header";
 import { SearchBox } from "../../components/SeachBox";
+import { MainCard } from "../../components/Cards";
 
 export const Dashboard = () => {
   return (
     <Box>
       <Header />
       <SearchBox />
+      <Grid
+        w={"100%"}
+        // m={["0px", "35px 60px"]}
+        mt={["20px", "0"]}
+        templateColumns={"repeat(auto-fill, minmax(420px, 1fr))"}
+        gap={10}
+        paddingX={["2", "8"]}
+      >
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(_ => (
+          <MainCard />
+        ))}
+      </Grid>
+      {/* <Flex flexWrap={"wrap"}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(_ => (
+          <MainCard />
+        ))}
+      </Flex> */}
     </Box>
   );
 };
